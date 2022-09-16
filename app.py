16119a3a6aee30208aa91f5c53e037bedd7cb76c
Martin Ma <z74ma@uwaterloo.ca>
2022-09-15 21:03:23 -0400

    Add baby naming features
    Create a web-based App using GPT-3 to work suggest names for a baby

--- app.py
+++ app.py
@@ -6,30 +6,37 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
+        baby = request.form["baby"]
+        response = get_openai_output(baby)
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def get_openai_output(baby):
+    """ Returns the output of the OpenAI API. """
+    return openai.Completion.create(
+        model="text-davinci-002",
+        prompt=generate_prompt(baby),
+        temperature=0.6,
+    )
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
+
+def generate_prompt(baby):
+    return """Suggest three names for a baby who is an Alien in outer space.
+
+baby: Harvard Professor
+Names: Doyle Sagittarius, Chen Zoodiac, Mars Rajpurkar
+baby: Brilliant
+Names: Martian Einstein, Newton Saturn, Aquarius Tesla
+baby: Basketball Player
+Names: James Capricorn, Jordan Venus, Meteor Durant
+
+baby: {}
 Names:""".format(
-        animal.capitalize()
+        baby.capitalize()
     )
